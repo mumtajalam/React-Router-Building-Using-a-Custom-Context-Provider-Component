d18ce4984c15dab6d84f0cfeedcc7166c8982153
Md Mumtaj Alam <98735031+mumtajalam@users.noreply.github.com>
2022-06-26 21:51:17 +0530

    create

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import { BackgroundColorChanger } from './Pages/BackgroundColorChanger'
-import { Calculator } from './Pages/Calculator'
-import { Index } from './Pages/Index'
+import { BackgroundColorChanger } from "./Pages/BackgroundColorChanger";
+import { Calculator } from "./Pages/Calculator";
+import { Index } from "./Pages/Index";
+import { Link, Route, Routes } from "react-router-dom";
 
-export const AppRoutes = () =>{
-    return (
-
-    )
-}
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route
+        path="/backgroundColorChanger"
+        element={<BackgroundColorChanger />}
+      />
+    </Routes>
+  );
+};
